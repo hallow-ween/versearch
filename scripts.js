d0139036ce1e97ff1ed68066a20ab257d86370bb
hallow.ween <sky608479@gmail.com>
2025-12-03 12:58:19 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ const verseName =
 document.getElementById("verseName");
 const verseText =
 document.getElementById("verseText");
+const findButtom =
+document.getElementById("findButton");
+const darklight =
+document.getElementById("darklight");
 
 function findVerse() {
 const bookvalue = 
@@ -17,7 +21,7 @@ fetch(`https://bible-api.com/${bookvalue}+${chapnversevalue}`)
 .then (response => response.json())
 .then (data => { bibleverse = data;
 if (data.error) {
-verseName.textContent = `${bookvalue} ${chapternversevalue} is invalid!`
+verseName.textContent = `${bookvalue} ${chapnversevalue} is invalid!`
 return;
 }
 console.log(data);
@@ -34,10 +38,18 @@ function darkOrLight() {
 if (!dark) {
 document.body.style.backgroundColor = "black";
 document.body.style.color = "white";
+findButton.style.backgroundColor = "black";
+darklight.style.backgroundColor = "black";
+bibleVerseAndChapter.style.backgroundColor = "black";
+bibleBooks.style.backgroundColor = "black";
 dark = true;
 } else {
 document.body.style.backgroundColor = "white";
 document.body.style.color = "black";
+findButton.style.backgroundColor = "white";
+darklight.style.backgroundColor = "white";
+bibleVerseAndChapter.style.backgroundColor = "white";
+bibleBooks.style.backgroundColor = "white";
 dark = false;
 }
 };
